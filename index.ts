25cda675ddc199ef9844e1296c0d3b829a623b03
simonachess <simonachess@gmail.com>
2021-12-01 13:33:14 +0200

    changed file names and fn names to more informative

--- index.ts
+++ index.ts
@@ -1,42 +1,3 @@
-// abstract class Employee {
-//     constructor(private firstName: string, private lastName: string) {
-//     }
-//     abstract getSalary(): number
-//     get fullName(): string {
-//         return `${this.firstName} ${this.lastName}`;
-//     }
-//     compensationStatement(): string {
-//         return `${this.fullName} makes ${this.getSalary()} a month.`;
-//     }
-// }
-
-// class FullTimeEmployee extends Employee {
-//     constructor(firstName: string, lastName: string, private salary: number) {
-//         super(firstName, lastName);
-//     }
-//     getSalary(): number {
-//         return this.salary;
-//     }
-// }
-
-// class Contractor extends Employee {
-//     constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
-//         super(firstName, lastName);
-//     }
-//     getSalary(): number {
-//         return this.rate * this.hours;
-//     }
-// }
-
-// let john = new FullTimeEmployee('John', 'Doe', 12000);
-// let jane = new Contractor('Jane', 'Doe', 100, 160);
-
-// console.log(john.compensationStatement());
-// console.log(jane.compensationStatement());
-
-// John Doe makes 12000 a month.
-// Jane Doe makes 16000 a month.
-
 const amountDOM: HTMLInputElement = <HTMLInputElement>document.getElementById("amount");
 const percentDOM: HTMLInputElement = <HTMLInputElement>document.getElementById("percent");
 const yearsDOM: HTMLInputElement = <HTMLInputElement>document.getElementById("years");
@@ -100,7 +61,7 @@ function calculateCredit(credit: Credit) {
     }
 }
 
-function calculateFC() {
+function calculateFastCredit() {
     let fcredit = new FastCredit(parseFloat(amountDOM.value), parseFloat(yearsDOM.value), parseFloat(percentDOM.value));
     calculateCredit(fcredit);
 }
@@ -151,7 +112,7 @@ class HouseCredit extends Credit {
     }
 }
 
-function calculateHC() {
+function calculateHouseCredit() {
     let hcredit = new HouseCredit(parseFloat(amountDOM.value), parseFloat(yearsDOM.value), parseFloat(percentDOM.value), parseFloat(salaryDOM.value), parseFloat(kidsDOM.value));
     calculateCredit(hcredit);
 }
